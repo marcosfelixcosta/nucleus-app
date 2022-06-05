cc6d2a13cbe614508e718bd62ad1615bf52306c1
Marcos Felix <marcosfelixcosta@gmail.com>
2022-06-05 18:09:04 -0300

    alt-Icon

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -16,7 +16,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+const Input: React.FC<InputProps> = ({ name, ...rest }) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -39,10 +39,12 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
       path: 'value',
     });
   }, [fieldName, registerField]);
+ // icon: Icon,
+ // {Icon && <Icon size={20} />}
 
   return (
     <Container isErrored={!!error} isFocused={isFocused} isFilled={isFilled}>
-      {Icon && <Icon size={20} />}
+    
       <input
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
